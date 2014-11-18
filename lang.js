4b4ce1171ed7d22b03f90a2cfef0b389f93d823a
Julian Kirsch <kirschju@sec.in.tum.de>
2014-11-18 22:11:50 +0100

    bla

--- lang.js
+++ lang.js
@@ -47,5 +47,5 @@ function activate_menu()
 {
 	b = document.getElementsByClassName("m_" + document.URL.replace(/^.*(\\|\/|\:)/, '').split('.')[0])
 	for (i = 0; i < b.length; i++)
-		b.setAttribute('class', 'active');
+		b[i].setAttribute('class', 'active');
 }
