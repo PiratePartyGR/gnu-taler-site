4bf637cfc66e6720bcf4c215994bb6c4f116ad72
Julian Kirsch <kirschju@sec.in.tum.de>
2014-11-18 17:10:57 +0100

    Lang

--- lang.js
+++ lang.js
@@ -14,15 +14,24 @@ function setLang(l)
 	}
 }
 
+function get_default_lang()
+{
+	for (l in navigator.languages) {
+		for (j in ['en', 'de', 'fr']) {
+			if (l.search(j) != -1)
+				return j;
+		}
+	}
+
+	return 'en';
+}
 function loadLang()
 {
+	l = '';
 	if (supports_html5_storage()) {
 		l = sessionStorage.getItem('lang');
-		if (l == '') {
-			l = navigator.userLanguage || navigator.language;
-		}
-	} else {
-		l = navigator.userLanguage || navigator.language;
 	}
+
+	if (l == '') l = get_default_lang();
 	setLang(l);
 }
